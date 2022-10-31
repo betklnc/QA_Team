ab75bde5008827e972d09626d8fe98c68725e344
Betul Kabadayi <betul.kabadayi@obss.com.tr>
2022-10-31 22:20:30 +0300

    Okunan response'tan bir ismin kontrolü yapıldı.

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -1,7 +1,9 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -13,28 +15,14 @@ public class Deneme {
 
     @Test
     public void Deneme () {
-
-        /*given()
-                .when()
-                .get("http://usersdemo.herokuapp.com/api/users?page=2")
-                .then()
-                .statusCode(200)
-                .statusLine("HTTP/1.1 200 OK")
-                .assertThat().body("total_pages",equalTo(4))
-                .assertThat().body("per_page",equalTo(3))
-                .log().all();
-
-    }*/
-        RestAssured.baseURI = "http://usersdemo.herokuapp.com/api/users?page=2";
+        // https://mocki.io/fake-json-api fake json api oluşturma
+        // {"status":"success","data":[{"id":1,"employee_name":"Tiger Nixon","employee_salary":320800,"employee_age":61,"profile_image":""},{"id":2,"employee_name":"Garrett Winters","employee_salary":170750,"employee_age":63,"profile_image":""},{"id":3,"employee_name":"Ashton Cox","employee_salary":86000,"employee_age":66,"profile_image":""},{"id":4,"employee_name":"Cedric Kelly","employee_salary":433060,"employee_age":22,"profile_image":""},{"id":5,"employee_name":"Airi Satou","employee_salary":162700,"employee_age":33,"profile_image":""},{"id":6,"employee_name":"Brielle Williamson","employee_salary":372000,"employee_age":61,"profile_image":""},{"id":7,"employee_name":"Herrod Chandler","employee_salary":137500,"employee_age":59,"profile_image":""},{"id":8,"employee_name":"Rhona Davidson","employee_salary":327900,"employee_age":55,"profile_image":""},{"id":9,"employee_name":"Colleen Hurst","employee_salary":205500,"employee_age":39,"profile_image":""},{"id":10,"employee_name":"Sonya Frost","employee_salary":103600,"employee_age":23,"profile_image":""},{"id":11,"employee_name":"Jena Gaines","employee_salary":90560,"employee_age":30,"profile_image":""},{"id":12,"employee_name":"Quinn Flynn","employee_salary":342000,"employee_age":22,"profile_image":""},{"id":13,"employee_name":"Charde Marshall","employee_salary":470600,"employee_age":36,"profile_image":""},{"id":14,"employee_name":"Haley Kennedy","employee_salary":313500,"employee_age":43,"profile_image":""},{"id":15,"employee_name":"Tatyana Fitzpatrick","employee_salary":385750,"employee_age":19,"profile_image":""},{"id":16,"employee_name":"Michael Silva","employee_salary":198500,"employee_age":66,"profile_image":""},{"id":17,"employee_name":"Paul Byrd","employee_salary":725000,"employee_age":64,"profile_image":""},{"id":18,"employee_name":"Gloria Little","employee_salary":237500,"employee_age":59,"profile_image":""},{"id":19,"employee_name":"Bradley Greer","employee_salary":132000,"employee_age":41,"profile_image":""},{"id":20,"employee_name":"Dai Rios","employee_salary":217500,"employee_age":35,"profile_image":""},{"id":21,"employee_name":"Jenette Caldwell","employee_salary":345000,"employee_age":30,"profile_image":""},{"id":22,"employee_name":"Yuri Berry","employee_salary":675000,"employee_age":40,"profile_image":""},{"id":23,"employee_name":"Caesar Vance","employee_salary":106450,"employee_age":21,"profile_image":""},{"id":24,"employee_name":"Doris Wilder","employee_salary":85600,"employee_age":23,"profile_image":""}],"message":"Successfully! All records has been fetched."}
+        RestAssured.baseURI = "https://mocki.io/v1/f8e4a903-f502-44bf-8709-c6266ff901c3";
         RequestSpecification httpRequest = RestAssured.given();
         Response response = httpRequest.get("");
+
         JsonPath jsonPathEvaluator = response.jsonPath();
-
-        List<Integer> alljson = jsonPathEvaluator.getList("data");
-        System.out.println(alljson);
-        for (int i=0; i<alljson.size(); i++ )
-        {
-            System.out.println(alljson.get(i));
-
-        }
-}}
+        List<String> names = jsonPathEvaluator.get("data.employee_name");
+        String name = names.get(1);
+        Assert.assertEquals(name, "Garrett Winters");
+    }}
